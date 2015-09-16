e58bd8bd3fa9bbfef801d953db85e8e1b2562a59
sorinmanole <somanole@gmail.com>
2015-09-16 11:00:02 +0200

    test commit

--- app/src/main/java/com/gaitproject/gait/Acceleration.java
+++ app/src/main/java/com/gaitproject/gait/Acceleration.java
@@ -9,7 +9,7 @@ public class Acceleration {
     private double z;
 
     public Acceleration(float x_value, float y_value, float z_value, long timestamp, long userid) {
-        x= new Double(""+x_value);
+        x= new Double(" "+x_value);
         y= new Double(""+y_value);
         z= new Double(""+z_value);
         this.timestamp = timestamp;
